cac490df8901531f55edc8cbd664bc7d03f8db32
CodeZhangBorui <zhangborui.2008@foxmail.com>
2024-02-05 09:06:04 +0800

    refactor(daemon): 简介逻辑，提取重用代码到函数

--- daemon.py
+++ daemon.py
@@ -22,79 +22,95 @@ logging.basicConfig(
 
 console = Console()
 
-# Load plugins
-command_executors = {}
-files = os.listdir("./plugins") # List all files in plugins directory
-for file in files:
-    if file.endswith(".py"):
-        try:
-            exec(f"import plugins.{file[:-3]} as {file[:-3]}") # Import the plugin
-            this = {
-                'name': file[:-3],
-                'author': eval(f"{file[:-3]}.plugin['author']"),
-                'version': eval(f"{file[:-3]}.plugin['version']"),
-                'export': eval(f"{file[:-3]}.plugin['export']")
-            }
-            logging.info(f"Loaded plugin [purple]{file}[/] ({this['version']}) by [purple]{this['author']}[/]", extra={"markup": True})
-            for cmd in this['export']:
-                command_executors[cmd] = this['name'] # Add the command to the list of executors
-        except Exception as e:
-            console.print_exception()
-            logging.error(f"Error while loading plugin {file}. See above.")
-    
-with open('plugins.json', 'w') as f:
-    json.dump({'command_executors': command_executors}, f, indent=4) # Save the executors to a file for the frontend
+def load_tasks(create_on_missing=False):
+    try:
+        # Load tasks
+        with open('tasks.json') as f:
+            return json.load(f)['tasks']
+    except FileNotFoundError:
+        if create_on_missing:
+            # Create default tasks.json file
+            with open('tasks.json', 'w') as f:
+                f.write('{"tasks": []}')
+            logging.warning("Default tasks.json file created")
+            return []
+        else:
+            return None
+    except json.JSONDecodeError:
+        # If the file is corrupted, crash
+        return -1
+    except KeyError:
+        # If the file is in wrong format
+        return -2
 
-try:
-    # Load tasks
-    with open('tasks.json') as f:
-        tasks = json.load(f)['tasks']
-except FileNotFoundError:
-    # Create default tasks.json file
-    with open('tasks.json', 'w') as f:
-        f.write('{"tasks": []}')
-    logging.warning("Default tasks.json file created")
-    tasks = []
-except json.JSONDecodeError:
-    # If the file is corrupted, crash
+def load_plugins(previous={}):
+    command_executors = previous
+    files = os.listdir("./plugins") # List all files in plugins directory
+    for file in files:
+        if file.endswith(".py") and file[:-3] not in previous.values():
+            try:
+                exec(f"import plugins.{file[:-3]} as {file[:-3]}") # Import the plugin
+                this = {
+                    'name': file[:-3],
+                    'author': eval(f"{file[:-3]}.plugin['author']"),
+                    'version': eval(f"{file[:-3]}.plugin['version']"),
+                    'export': eval(f"{file[:-3]}.plugin['export']")
+                }
+                logging.info(f"Loaded plugin [purple]{file}[/] ({this['version']}) by [purple]{this['author']}[/]", extra={"markup": True})
+                for cmd in this['export']:
+                    command_executors[cmd] = this['name'] # Add the command to the list of executors
+            except Exception as e:
+                console.print_exception()
+                logging.error(f"Error while loading plugin {file}. See above.")
+    with open('plugins.json', 'w') as f:
+        json.dump({'command_executors': command_executors}, f, indent=4) # Save the executors to a file for the frontend
+    return command_executors
+
+# Load tasks and plugins
+tasks = load_tasks(create_on_missing=True)
+if tasks == -1:
     logging.critical("JSON file is corrupted")
     crash(-1)
-except KeyError:
-    # If the file is in wrong format, crash
+elif tasks == -2:
     logging.critical("Wrong format in JSON file")
-    crash(-1)
+    crash(-2)
+
+command_executors = load_plugins()
 
 logging.info("Daemon started")
 logging.info(f"Loaded {len(tasks)} tasks")
 logging.info(f"Loaded {len(command_executors)} executors")
 
 while True:
-    while datetime.datetime.now().second != 0: # Wait for the next minute
+    # Wait for the next minute
+    while datetime.datetime.now().second != 0:
         try:
             sleep(1)
         except KeyboardInterrupt:
             # Gracefully exit on Ctrl+C
             logging.warning("KeyboardInterrupt received, shutting down daemon...")
             exit(0)
-    try:
-        # Check to see if tasks.json has been modified
-        with open('tasks.json') as f:
-            ntasks = json.load(f)['tasks']
-        logging.debug(f"Read tasks.json file successfully")
-    except FileNotFoundError:
+            
+    # Refresh tasks and plugins
+    ntasks = load_tasks()
+    if ntasks == None:
         logging.warning("Could not refresh tasks as tasks.json file is missing. Rerun the daemon to create it.")
-    except json.JSONDecodeError:
+    elif ntasks == -1:
         logging.warning("Could not refresh tasks as tasks.json file is corrupted")
-    except KeyError:
+    elif ntasks == -2:
         logging.warning("Could not refresh tasks as tasks.json file is in wrong format")
-    finally:
-        if ntasks != tasks:
-            # If the tasks have changed, reload them
-            logging.warning(f"Detect task changes, reloading.")
-            logging.info(f"Loaded {len(ntasks)} tasks")
-            tasks = ntasks
+    elif ntasks != tasks:
+        # If the tasks have changed, reload them
+        logging.warning(f"Detect task changes, reloading.")
+        logging.info(f"Loaded {len(ntasks)} tasks")
+        tasks = ntasks
+    command_executors = load_plugins(command_executors)
+    
+    # Get current time
     nowtime = datetime.datetime.now().strftime("%H:%M")
     logging.debug(f"Current time: {nowtime}")
+    
+    # Execute tasks
     for task in tasks:
         if task['time'] == nowtime:
             logging.info(f"Executing task id {task['id']}")
@@ -108,8 +124,8 @@ while True:
             elif task['type'] == "plugin":
                 # Call the plugin
                 try:
-                    executor = task['command'].split(':')[0]
                     plug = command_executors[executor]
+                    executor = task['command'].split(':')[0]
                     args = ' '.join(task['command'].split(':')[1:])
                     ret = eval(f"{plug}.{executor}({args})")
                     logging.info(f"Executor finished with return {ret}.")
