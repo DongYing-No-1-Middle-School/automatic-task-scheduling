55bcc77c3e60452717b44301ccb0e95b61b0b0b7
CodeZhangBorui <zhangborui.2008@foxmail.com>
2024-02-05 09:20:53 +0800

    perf(frontend): 改善frontend添加任务时的体验

--- frontend.py
+++ frontend.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import datetime
 
 from rich import print
 from rich.markup import escape
@@ -26,46 +27,48 @@ if args[1] == "help":
     print_usage()
     exit(0)
 elif args[1] == "add":
-    if len(args) < 4:
+    if len(args) < 3:
         # Not enough arguments
         print(f"[red bold]Error:[/red bold] Task details must be provided.")
         print(f"")
-        print(f"[blue bold]Usage:[/] frontend.py add <[purple]time[/]> (-p) <[purple]command|plugin[green bold]:[/]args[/]>")
+        print(f"[blue bold]Usage:[/] frontend.py add <[purple]time[/]> <[purple]command|plugin>")
         print(f"- [purple]time[/] must be in the format [blue bold]HH:MM[/].")
-        print(f"- [purple]-p[/] is an optional argument to run the command with [blue bold]plugin executor[/].")
         exit(1)
     time = args[2]
-    if len(time) != 5 or time[2] != ":":
-        # Simple time format check
+    try:
+        # Check if time is in the correct format
+        atetime.strptime(time, "%H:%M")
+    except ValueError:
         print(f"[red bold]Error:[/red bold] Invalid time format. Time must be in the format [blue bold]HH:MM[/].")
         exit(1)
-    if args[3] == "-p":
-        # Type of task is plugin
-        tasktype = "plugin"
-        command = " ".join(args[4:])
-        executor = command.split(":")[0]
-        args = command.split(":")[1]
+    tasktype = args[3]
+    if tasktype == 'command':
+        # Get the command
+        cmd = input("Enter the command\n> ")
+    elif tasktype == 'plugin':
+        # Check if the plugin is in the list of executors
         with open("plugins.json", "r") as f:
             plugins = json.load(f)["command_executors"]
         if executor not in plugins.keys():
-            print(f"[red bold]Error:[/red bold] Executor '{executor}' invaild.")
+            print(f"[red bold]Error:[/red bold] Executor '{executor}' invaild. Initalize the plugin by running daemon once.")
             exit(1)
-    else:
-        # Type of task is command
-        command = " ".join(args[3:])
-        tasktype = "command"
+        # Get the plugin and arguments
+        plug = input("Enter the plugin/executor\n> ")
+        args = input("Enter the arguments of the plugin (Use ',' to seperate)\n> ")
+        cmd = f"{plug}:{args}"
     # Print task details
     print(f"[bold]Adding a new task:[/]")
     print(f"  [blue bold]Time:[/] {time}")
     print(f"  [blue bold]Type:[/] {tasktype}")
-    print(f"  [blue bold]Command:[/] '{command}'")
+    print(f"  [blue bold]Command/Plug:[/] '{cmd}'")
+    # Add the task to file
     with open("tasks.json", "r") as f:
         tasks = json.load(f)["tasks"]
     if len(tasks) == 0:
         id = 1
     else:
         id = tasks[-1]["id"] + 1
-    tasks.append({"id":id,"time":time,"command":command, "type":tasktype})
+    tasks.append({"id":id,"time":time,"command":cmd, "type":tasktype})
     with open("tasks.json", "w") as f:
         json.dump({"tasks":tasks}, f)
     print(f"[green bold]Task added successfully.[/] (ID: {id})")
@@ -73,11 +76,12 @@ elif args[1] == "add":
 elif args[1] == "remove":
     if len(args) < 3:
         # Not enough arguments
-        print(f"[red bold]Error:[/red bold] Task ID must be provided.")
+        print(f"[red bold]Error:[/red bold] Task ID must be provided. Use [blue bold]frontend list[/] to get the ID of the task to remove.")
         print(f"")
         print(f"[blue bold]Usage:[/] frontend.py remove <[purple]id[/]>")
         exit(1)
     id = int(args[2])
+    # Remove the task from file
     with open("tasks.json", "r") as f:
         tasks = json.load(f)["tasks"]
     for task in tasks:
@@ -101,12 +105,14 @@ elif args[1] == "list":
             print(f"[red bold]Error:[/red bold] Invalid sorting method. Use [blue bold]time[/] [grey](default)[/] or [blue bold]id[/] followed.")
             exit(1)
     tasks.sort(key=lambda task: task["time"])
+    # Construct the rich table
     table = Table(title="Tasks")
     table.add_column("ID", style="blue bold")
     table.add_column("Time", style="blue bold")
     table.add_column("Command", style="blue bold")
     for task in tasks:
         table.add_row(str(task["id"]), task["time"], task["command"])
+    # Print the table
     print(table)
     exit(0)
 else:
